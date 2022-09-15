20462c9f6748ec8fa232fa532d9c047bf6050e74
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-15 15:59:49 -0300

    feat: Add cars routes to the app.
    Next the errorhandlers will be created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
+import carRouter from './routes/Car';
 
 const app = express();
+app.use(express.json());
+app.use(carRouter);
 
 export default app;
