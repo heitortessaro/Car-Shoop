4943686d2bdc72aab0bdd2c9e907ed73f7ed623b
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-15 18:11:51 -0300

    fix: Fix param id call on Car routes.

--- src/routes/Car.ts
+++ src/routes/Car.ts
@@ -13,11 +13,11 @@ route.post('/cars', (req: Request, res: Response) =>
   carController.create(req, res));
 route.get('/cars', (req: Request, res: Response) =>
   carController.read(req, res));
-route.get('/cars/id', (req: Request, res: Response) =>
+route.get('/cars/:id', (req: Request, res: Response) =>
   carController.readOne(req, res));
-route.put('/cars/id', (req: Request, res: Response) =>
+route.put('/cars/:id', (req: Request, res: Response) =>
   carController.update(req, res));
-route.delete('/cars/id', (req: Request, res: Response) =>
+route.delete('/cars/:id', (req: Request, res: Response) =>
   carController.delete(req, res));
 
 export default route;
