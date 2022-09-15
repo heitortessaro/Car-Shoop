f18bf0df444b6b8eff57f2f1d5005bd39a82bf0e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-15 15:55:49 -0300

    feat: Create cars route.
    It has routes to create, read all, read one, update, and delete car registers on the db.

---
+++ src/routes/Car.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from 'express';
+import CarController from '../controllers/Car';
+import CarService from '../services/Car';
+import CarModel from '../models/Cars';
+
+const route = Router();
+
+const car = new CarModel();
+const carService = new CarService(car);
+const carController = new CarController(carService);
+
+route.post('/cars', (req: Request, res: Response) =>
+  carController.create(req, res));
+route.get('/cars', (req: Request, res: Response) =>
+  carController.read(req, res));
+route.get('/cars/id', (req: Request, res: Response) =>
+  carController.readOne(req, res));
+route.put('/cars/id', (req: Request, res: Response) =>
+  carController.update(req, res));
+route.delete('/cars/id', (req: Request, res: Response) =>
+  carController.delete(req, res));
+
+export default route;
