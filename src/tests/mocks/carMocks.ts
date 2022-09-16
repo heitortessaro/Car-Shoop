1c7d719e460170adc9c1bca4d564baf6ffb7d78e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-16 10:37:55 -0300

    test: Create mocks for cars route.

---
+++ src/tests/mocks/carMocks.ts
@@ -0,0 +1,29 @@
+import { ICar } from "../../interfaces/ICar";
+
+const carMock: ICar = {
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+}
+
+const carMockWithId: ICar & { _id: string } = {
+  _id: '6323930286e451c2a5727b9d',
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+}
+
+const carMockForChange: ICar = {
+  model: "Fusca",
+  year: 1963,
+  color: "azul",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+}
