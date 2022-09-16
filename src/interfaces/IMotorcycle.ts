47072bcf70601d2a2e577fe59fde8a77a79ae298
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-16 10:48:53 -0300

    feat: Create interface IMotorcycle.
    It is an extension of the IVehicle interface.

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const MotorcycleZodSchema = VehicleZodSchema.extend({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number().int().gt(0).lte(2500),
+});
+
+type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
+
+export { MotorcycleZodSchema, IMotorcycle };
