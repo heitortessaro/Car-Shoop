51c313a7d6345416ef1be1dea9cb299a7a56db9d
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-16 13:12:15 -0300

    feat: Create router for /motorcycle.

---
+++ src/routes/Motorcycle.ts
@@ -0,0 +1,25 @@
+import { Router, Request, Response } from 'express';
+import MotorcycleController from '../controllers/Motorcycle';
+import MotorcycleService from '../services/Motorcycle';
+import MotorcycleModel from '../models/Motorcycle';
+
+const route = Router();
+
+const motorcycle = new MotorcycleModel();
+const motorcycleService = new MotorcycleService(motorcycle);
+const motorcycleController = new MotorcycleController(motorcycleService);
+
+const baseRoute = '/motorcycle';
+
+route.post(baseRoute, (req: Request, res: Response) =>
+  motorcycleController.create(req, res));
+route.get(baseRoute, (req: Request, res: Response) =>
+  motorcycleController.read(req, res));
+route.get(`${baseRoute}/:id`, (req: Request, res: Response) =>
+  motorcycleController.readOne(req, res));
+route.put(`${baseRoute}/:id`, (req: Request, res: Response) =>
+  motorcycleController.update(req, res));
+route.delete(`${baseRoute}/:id`, (req: Request, res: Response) =>
+  motorcycleController.delete(req, res));
+
+export default route;
